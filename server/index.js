8fa14d1e7a0fd7f3d769e9414c14a6b492c38d8a
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-12-21 14:58:07 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const path = require('path')
+
+app.use('/', express.static(path.join(__dirname, '../client/dist')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  // res.send('Hello World!!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
